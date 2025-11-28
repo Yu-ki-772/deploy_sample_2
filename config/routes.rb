9123db0d98a2195c5d21d45dc9351be7158118b4
Yu-ki-772 <yu-ki@DESKTOP-N94U91P.localdomain>
2025-11-28 15:24:29 +0900

    fix：lint、scan_rubyでのエラー部分の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "diagnoses/start"
   get "diagnoses/questions"
   get "diagnoses/result"
-  root 'diagnoses#start'
+  root "diagnoses#start"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
