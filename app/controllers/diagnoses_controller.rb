12661cb23ce734415639c496c56502f99568a5ea
Yu-ki-772 <yu-ki@DESKTOP-N94U91P.localdomain>
2025-11-28 13:37:53 +0900

    トップ画面等作成

---
+++ app/controllers/diagnoses_controller.rb
@@ -0,0 +1,10 @@
+class DiagnosesController < ApplicationController
+  def start
+  end
+
+  def questions
+  end
+
+  def result
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
-  root 'posts#index'
+  get "diagnoses/start"
+  get "diagnoses/questions"
+  get "diagnoses/result"
+  root 'diagnoses#start'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
